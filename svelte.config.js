4768cb135669ffe4930f1fb50ccefde2395401fa
David Serrano <75829857+serranoio@users.noreply.github.com>
2024-03-02 17:31:14 -0600

    switched config

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,16 @@
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
 import adapter from '@sveltejs/adapter-netlify';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+function isWebComponentSvelte(code) {
+    const svelteOptionsIdx = code.indexOf('<svelte:options ')
+    if(svelteOptionsIdx < 0) {
+        return false
+    }
+    const tagOptionIdx = code.indexOf('tag=', svelteOptionsIdx)
+    const svelteOptionsEndIdx = code.indexOf('>',svelteOptionsIdx);
+    return tagOptionIdx > svelteOptionsIdx && tagOptionIdx < svelteOptionsEndIdx
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,8 +22,28 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter:  adapter({
+			// if true, will create a Netlify Edge Function rather
+			// than using standard Node-based functions
+			edge: false,
+
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app.
+			// if `edge` is true, this option cannot be used
+			split: false
+		})
+	},
+	vitePlugin: {
+        experimental: {
+            dynamicCompileOptions({code}) {
+                if(isWebComponentSvelte(code)) {
+                    return {
+                        customElement: true
+                    }
+                }
+            }
+        }
+    }
 };
 
 export default config;
