228e5b191e566c52952293790bb01e2f75ad5f53
David Serrano <75829857+serranoio@users.noreply.github.com>
2024-03-18 10:03:20 -0400

    fixed hopefully

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,22 @@
 <script>
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
-  import About from '../components/About.svelte';
-  import Header from './Header.svelte';
-  import Hero from '../components/Hero.svelte';
-  import Routine from '../components/Routine.svelte';
-  import Gallery from '../components/Gallery.svelte';
+import About from "./About.svelte";
+import Gallery from "./Gallery.svelte";
+import Hero from "./Hero.svelte";
+import Routine from "./Routine.svelte";
+
 </script>
 
 <svelte:head>
 	<title>David Serrano</title>
 	<meta name="description" content="Svelte demo app" />
 	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400..900&display=swap" rel="stylesheet">
 </svelte:head>
 <Hero/>
 <About/>
-<Routine/>
 <Gallery/>
+<Routine/>
 <style>
 
 </style>
